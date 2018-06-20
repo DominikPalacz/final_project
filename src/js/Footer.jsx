dcf5cf3496c2272c6b132575f4ab54aac974950a
palacz.dominik@gmail.com <palacz.dominik@gmail.com>
2018-06-20 14:15:41 +0200

    Component divided

---
+++ src/js/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Footer extends React.Component {
+
+    render() {
+        return (
+            <footer className="HolyGrail-footer">
+                <p>&copy; 2018 by Dominik Palacz</p>
+            </footer>
+        )
+    }
+}
+
+module.exports = Footer;
---
+++ src/js/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Header extends React.Component {
+
+
+    render() {
+        return (
+            <header className="HolyGrail-header">
+                <h1>Najlpeszy symulator girłdy BITCOIN</h1>
+                <img src="../../imgages/btc.png" alt=""/>
+            </header>
+        )
+    }
+}
+
+module.exports = Header;
